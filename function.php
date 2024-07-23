493fe812002de6747629f1903c2090a5798b228b
Galih Rizky Fahrezy <galihrizkyfahrezy@gmail.com>
2024-07-23 10:57:02 +0700

    Update function.php

--- function.php
+++ function.php
@@ -1,15 +1,16 @@
-//program php ini menggunakan framework laravel
-
-public function denda($anggota_id)
-{
-    $denda = DB::select("SELECT user.id as id, user.nama as nama, peminjaman.buku_id as buku_id,DATEDIFF(tanggal_batas_kembali, tanggal_pinjam) as batas_pinjam ,IF(DATEDIFF(tanggal_kembali, tanggal_batas_kembali)>0, 
-    DATEDIFF(tanggal_kembali, tanggal_batas_kembali)* 1000, 0) as denda from peminjaman inner join user
-    on user.id = peminjaman.anggota_id peminjaman.anggota_id =".$anggota_id);
-    $totaldenda = 0;
-    foreach($denda as $tambahdenda)
+    public function denda($anggota_id)
     {
-        $totaldenda = $totaldenda + $tambahdenda->denda;
-    }
+        $denda = DB::select("SELECT peminjaman.id as id, user.nama as nama, peminjaman.buku_id as buku_id,DATEDIFF(tanggal_pinjam+14, tanggal_pinjam) as batas_pinjam ,IF(DATEDIFF(tanggal_kembali, tanggal_pinjam+14)>0, 
+        DATEDIFF(tanggal_kembali, tanggal_batas_kembali)* 1000, 0) as denda from peminjaman inner join user
+        on user.id = peminjaman.anggota_id where peminjaman.anggota_id =".$anggota_id);
 
-    return view('denda',compact('denda','totaldenda'));
-}
+        $totaldenda = 0;
+
+        foreach($denda as $tambahdenda)
+        {
+            if($tambahdenda->denda > 0)
+            $totaldenda = $totaldenda + $tambahdenda->denda;
+        }
+
+        return view('denda',compact('denda','totaldenda'));
+    }
