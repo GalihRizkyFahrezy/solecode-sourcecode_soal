d20e4eedf7b7c5821c08866a17302aa3f43ffa84
Galih Rizky Fahrezy <galihrizkyfahrezy@gmail.com>
2024-07-23 10:35:15 +0700

    Update function.php

--- function.php
+++ function.php
@@ -4,7 +4,7 @@ public function denda($anggota_id)
 {
     $denda = DB::select("SELECT user.id as id, user.nama as nama, peminjaman.buku_id as buku_id,DATEDIFF(tanggal_batas_kembali, tanggal_pinjam) as batas_pinjam ,IF(DATEDIFF(tanggal_kembali, tanggal_batas_kembali)>0, 
     DATEDIFF(tanggal_kembali, tanggal_batas_kembali)* 1000, 0) as denda from peminjaman inner join user
-    on user.id = peminjaman.anggota_id where denda > 0 && peminjaman.anggota_id =".$anggota_id);
+    on user.id = peminjaman.anggota_id peminjaman.anggota_id =".$anggota_id);
     $totaldenda = 0;
     foreach($denda as $tambahdenda)
     {
